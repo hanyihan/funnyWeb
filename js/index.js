36fab860bf56306e870490685871ed3bfa057c3a
hanyihan <steven.han.yi@foxmail.com>
2018-09-27 15:14:05 +0800

    page2 and page3 done

--- js/index.js
+++ js/index.js
@@ -13,16 +13,71 @@ window.onload=function(){
 	var cliNodes = document.querySelectorAll("#content .list > li");
 	var clist = document.querySelector('#content .list');
 	console.log(clist);
-	var now = 0;
 	
+	var now = 0;
+	var timer = 0;
+	
+	//视窗改变时改变ul的top值完成动画切换。同时导航栏完成切换
 	window.onresize = function(){
 		contentbind();
 		clist.style.top = -now*(document.documentElement.clientHeight - head.offsetHeight) + 'px';
 		arrow.style.left = liNodes[now].offsetLeft + liNodes[now].offsetWidth / 2 - arrow.offsetWidth / 2 + 'px';
 	}
 	
-	contentbind();
+	//为content 添加滚动监听时间
+	//Firefox
+	if(content.addEventListener) {
+		content.addEventListener("DOMMouseScroll",function(ev){
+			ev = ev || event;
+			clearTimeout(timer);
+			//这个延时保证用户滚多次下，页面只切换一次
+			timer = setTimeout(function(){
+				fn(ev);
+			},200)
+		});
+	}
 	
+	//IE / Chrome
+	content.onmousewheel = function(ev){
+		ev = ev || event;
+		clearTimeout(timer);
+		timer = setTimeout(function() {
+			fn(ev);
+		}, 200)
+	}
+	
+	//判断滚轮方向以及与页面滚动联动
+	function fn(ev) {
+		ev = ev || event;
+		var dir = "";
+		//IE / Chrome
+		if(ev.wheelDelta) {	
+			dir = ev.wheelDelta > 0? "up" : "down";
+		}
+		
+		//Firefox
+		else if (ev.detail) {
+			dir = ev.detail < 0? "up" : "down";
+		}
+		
+		switch(dir) {
+			case "up":
+				if(now > 0) {
+					now--;
+					move(now);
+				}
+				break;
+			case "down":
+				if(now < cliNodes.length - 1) {
+					now++;
+					move(now);
+				}
+		}
+	}
+	
+	
+	//动态计算content内容的高度
+	contentbind();
 	function contentbind(){
 		content.style.height = document.documentElement.clientHeight - head.offsetHeight + 'px';
 		for(let i = 0; i < cliNodes.length; i++ ){
@@ -30,8 +85,8 @@ window.onload=function(){
 		}
 	}
 	
+	//导航的绑定切换
 	headbind();
-	
 	function headbind(){
 		firstUpNodes.style.width = '100%';
 		arrow.style.left = firstLiNodes.offsetLeft + firstLiNodes.offsetWidth / 2 - arrow.offsetWidth / 2 + 'px';
@@ -44,15 +99,18 @@ window.onload=function(){
 				now = this.index;
 			}
 			
-			//。up 切换 和 移动arrow
-			function move (index){
-				for(var j = 0; j < upNodes.length; j++) {
-					upNodes[j].style.width = '0';
-				}
-				upNodes[index].style.width = '100%';
-				arrow.style.left = liNodes[index].offsetLeft + liNodes[index].offsetWidth / 2 - arrow.offsetWidth / 2 + 'px';
-				clist.style.top = -index*(document.documentElement.clientHeight - head.offsetHeight)+"px";
-			}
+			
 		}
+	}
+	move(1);
+	
+	//up 切换 和 移动arrow
+	function move (index){
+		for(var j = 0; j < upNodes.length; j++) {
+			upNodes[j].style.width = '0';
+		}
+		upNodes[index].style.width = '100%';
+		arrow.style.left = liNodes[index].offsetLeft + liNodes[index].offsetWidth / 2 - arrow.offsetWidth / 2 + 'px';
+		clist.style.top = -index*(document.documentElement.clientHeight - head.offsetHeight)+"px";
 	}
 }
