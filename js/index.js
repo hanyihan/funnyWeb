534e9ada3045f49a212cce067e85cdc69b469a4e
hanyihan <steven.han.yi@foxmail.com>
2018-09-29 17:54:37 +0800

    done page one

--- js/index.js
+++ js/index.js
@@ -12,10 +12,118 @@ window.onload=function(){
 	var content = document.querySelector("#content");
 	var cliNodes = document.querySelectorAll("#content .list > li");
 	var clist = document.querySelector('#content .list');
-	console.log(clist);
+	
+	//第一屏
+	var home1liNodes =  document.querySelectorAll("#content .list .home .home1 > li");
+	var home2liNodes =  document.querySelectorAll("#content .list .home .home2 > li");
+	var home1 = document.querySelector("#content .list .home .home1");
+	
 	
 	var now = 0;
 	var timer = 0;
+	
+	var oldIndex = 0;
+	var timer3D = "dhaoengha";
+	var autoIndex = 0;
+	
+	
+	
+	//第一屏点击切换
+	home3D();
+	function home3D() {
+		for(var i = 0; i < home2liNodes.length; i++) {
+			home2liNodes[i].index = i;
+			home2liNodes[i].onclick = function(){
+				clearInterval(timer3D);
+				//循环将所有li的背景去掉
+				for(var k = 0; k< home2liNodes.length;k++) {
+					home2liNodes[k].classList.remove("on");
+				}
+				//为当前点击的li 添加背景
+				this.classList.add("on");
+				
+				//判断 当前点击的li比老的li大小，大于则为当前li添加rightshow，小于则添加leftshow
+				
+				if(this.index > oldIndex) {
+					home1liNodes[this.index].classList.remove("leftshow");
+					home1liNodes[this.index].classList.remove("righthide");
+					home1liNodes[this.index].classList.remove("lefthide");
+					
+					home1liNodes[this.index].classList.add("rightshow");
+					
+					home1liNodes[oldIndex].classList.remove("leftshow");
+					home1liNodes[oldIndex].classList.remove("rightshow");
+					home1liNodes[oldIndex].classList.remove("righthide");
+					
+					home1liNodes[oldIndex].classList.add("lefthide");
+					
+				}
+				
+				if(this.index < oldIndex) {
+					home1liNodes[this.index].classList.remove("rightshow");
+					home1liNodes[this.index].classList.remove("righthide");
+					home1liNodes[this.index].classList.remove("lefthide");
+					
+					home1liNodes[this.index].classList.add("leftshow");
+					
+					home1liNodes[oldIndex].classList.remove("leftshow");
+					home1liNodes[oldIndex].classList.remove("rightshow");
+					home1liNodes[oldIndex].classList.remove("lefthide");
+					
+					home1liNodes[oldIndex].classList.add("righthide");
+					
+				}
+				
+				//切换后当前的就变成了老的
+				oldIndex = this.index;
+				
+				
+				//自动和手动联系，需要让自动的知道当前显示的是哪一个
+				autoIndex = this.index;
+			}
+		}
+	}
+	
+	move3D();
+	function move3D() {
+		clearInterval(timer3D);
+		timer3D = setInterval(function(){
+			autoIndex++;
+			
+			if(autoIndex == home1liNodes.length) {
+				autoIndex = 0;
+			}
+			
+			for(var k = 0; k < home2liNodes.length; k++) {
+				home2liNodes[k].classList.remove("on");
+			}
+			
+			home2liNodes[autoIndex].classList.add("on");
+			
+			home1liNodes[autoIndex].classList.remove("leftshow");
+			home1liNodes[autoIndex].classList.remove("righthide");
+			home1liNodes[autoIndex].classList.remove("lefthide");
+			
+			home1liNodes[autoIndex].classList.add("rightshow");
+			
+			home1liNodes[oldIndex].classList.remove("leftshow");
+			home1liNodes[oldIndex].classList.remove("rightshow");
+			home1liNodes[oldIndex].classList.remove("righthide");
+			
+			home1liNodes[oldIndex].classList.add("lefthide");
+			
+			oldIndex = autoIndex;
+		},2000)
+	}
+	
+	home1.onmouseenter = function(){
+		clearInterval(timer3D);
+	}
+	
+	home1.onmouseleave = function(){
+		move3D();
+	}
+	
 	
 	//视窗改变时改变ul的top值完成动画切换。同时导航栏完成切换
 	window.onresize = function(){
